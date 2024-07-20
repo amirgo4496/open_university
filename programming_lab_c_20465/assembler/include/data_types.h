b113342f3cbf85a8ca7f42ef62f023f0d6836a95
amirgo4496 <amirgo4496@gmail.com>
2024-07-20 10:44:07 +0300

    test gitignore

--- programming_lab_c_20465/assembler/include/data_types.h
+++
@@ -1,5 +0,0 @@
-#ifndef __DATA_TYPES__
-#define __DATA_TYPES__
-
-
-#endif
--- programming_lab_c_20465/assembler/test/compile_pre_asm_test.sh
+++
@@ -1,2 +0,0 @@
-gcc -g pre_assembler_test.c ../src/slist.c ../src/pre_assembler.c ../src/errors.c ../src/hash_table.c ../src/utils.c ../src/operations.c -ansi -Wall -pedantic
-
